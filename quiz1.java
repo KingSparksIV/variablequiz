f72fc4d0ceec7a14e284f7ece442dd56bc68a15b
KingSparksIV <55165426+KingSparksIV@users.noreply.github.com>
2020-10-19 16:50:08 -0400

    Create quiz1.java

---
+++ quiz1.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String name;
+	name = "James Harden";
+	int num;
+	num = 13;
+	float ppg;
+	ppg = (float) 25.2;
+	boolean mvp;
+	mvp = true;
+
+
+	System.out.println("The Greatest Basketball Player Ever!");
+	System.out.println("Name: " + name);
+	System.out.println("Jersey Number: " + num);
+	System.out.println("Career PPG: " + ppg);
+	System.out.println("(T/F) Was James Harden ROBBED of the MVP trophy for the '18-'19 NBA season? "+ mvp);
+    }
+}
